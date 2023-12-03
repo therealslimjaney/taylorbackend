ca8fba1478768096153cfb30d11899c132fa3b2f
Jane Swingler <130300157+therealslimjaney@users.noreply.github.com>
2023-12-03 15:20:00 -0800

    Update UserRecordRepository.java

--- src/main/java/com/example/demo/UserRecordRepository.java
+++ src/main/java/com/example/demo/UserRecordRepository.java
@@ -1,16 +1,26 @@
 package com.example.demo;
-import java.util.Optional;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import com.google.cloud.spring.data.datastore.repository.DatastoreRepository;
 
+import java.util.Optional;
+
+/**
+ * Repository interface for managing user records in the Datastore.
+ */
 public interface UserRecordRepository extends DatastoreRepository<UserRecord, Long> {
 
+    /**
+     * Retrieves a user record based on the Google ID.
+     *
+     * @param userGoogleId The Google ID of the user.
+     * @return Optional containing the user record, or empty if not found.
+     */
     Optional<UserRecord> findByUserGoogleId(String userGoogleId);
 
+    /**
+     * Deletes a user record based on the ID.
+     *
+     * @param id The ID of the user record to be deleted.
+     */
     void deleteById(Long id);
-
 }
